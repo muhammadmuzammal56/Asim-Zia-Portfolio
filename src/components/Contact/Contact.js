41691742c7b9f71ce012e14185efde20fcddc2b2
Muhammad Muzammal <muhammadmuzammal56@gmail.com>
2025-06-27 11:33:41 +0500

    Added Tab icon and Open Send Email tab with EMail on Next Tab

--- src/components/Contact/Contact.js
+++ src/components/Contact/Contact.js
@@ -5,25 +5,29 @@ const Contact = () => {
   if (!contact.email) return null
 
   return (
-    <section className='section contact center' id='contact' style={{display: 'flex', flexDirection: 'column', alignItems: 'center'}}>
+    <section className='section contact center' id='contact' style={{ display: 'flex', flexDirection: 'column', alignItems: 'center' }}>
       <h2 className='section__title'>Contact</h2>
       <div className="contact__options">
-  <a href={`mailto:${contact.email}`}>
-    <span type='button' className='btn btn--outline'>
-      Email me
-    </span>
-  </a>
-  <a href={`tel:${contact.mobile.replace(/\s/g, '')}`}>
-    <span type='button' className='btn btn--outline'>
-      Make a Call
-    </span>
-  </a>
-  <a href={`https://api.whatsapp.com/send?phone=${contact.mobile.replace(/\s/g, '').replace('+', '')}`} target="_blank" rel="noopener noreferrer">
-    <span type='button' className='btn btn--outline'>
-      Whatsapp me
-    </span>
-  </a>
-</div>
+        <a
+          href="https://mail.google.com/mail/?view=cm&fs=1&to=asimziabhatti565@gmail.com"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          <span type="button" className="btn btn--outline">
+            Email me
+          </span>
+        </a>
+        <a href={`tel:${contact.mobile.replace(/\s/g, '')}`}>
+          <span type='button' className='btn btn--outline'>
+            Make a Call
+          </span>
+        </a>
+        <a href={`https://api.whatsapp.com/send?phone=${contact.mobile.replace(/\s/g, '').replace('+', '')}`} target="_blank" rel="noopener noreferrer">
+          <span type='button' className='btn btn--outline'>
+            Whatsapp me
+          </span>
+        </a>
+      </div>
 
     </section>
   )
